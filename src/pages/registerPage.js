6dbcce9bba4ab334b37cd8ba5384f7fcc2d91f6c
Owen O'Donnell <oodonne@tcd.ie>
2023-02-18 15:56:53 +0000

    Add intial register page

---
+++ src/pages/registerPage.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import RegisterForm from '../components/forms/registerForm';
+
+export default function RegisterPage() {
+
+  //get random number in range to vary image on each page refresh
+  const number = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
+  console.log(number)
+  //assign image to variable based on random numer
+  const image = `${process.env.PUBLIC_URL}/assets/login/cinema-${number}.jpg`;
+
+  return (
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <RegisterForm/>
+      </Grid>
+  );
+}
