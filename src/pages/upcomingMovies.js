71bdab29cbaad169a61f439a4af3eaf6c86045c7
Owen O'Donnell <oodonne@tcd.ie>
2023-02-19 16:22:49 +0000

    Add upcoming movies page

---
+++ src/pages/upcomingMovies.js
@@ -0,0 +1,82 @@
+import { Grid, Typography, Box } from "@mui/material";
+import React from "react";
+import Footer from "../components/siteFooter/footer";
+import FilterMovies from "../components/filterMovies/filterMovies";
+import MovieCard from "../components/movieCards/movieCard";
+import MoviePagination from "../components/pagination/pagination";
+import { useQuery } from "react-query";
+import { CircularProgress } from "@mui/material";
+import { getUpcomingMovies } from "../api/movie-api";
+import "@fontsource/righteous";
+
+const UpcomingMoviesPage = () => {
+
+  const {
+    data: upcomingData,
+    error,
+    isLoading,
+    isError,
+  } = useQuery(["Upcoming", { page: 1 }], getUpcomingMovies);
+
+  if (isLoading) {
+    return (
+      <div sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <span>
+        <h1>There was an error </h1>
+        {error.map((e) => (e ? <h1>Error Message: {e.message}</h1> : null))}
+      </span>
+    );
+  }
+
+  const movies = upcomingData.results;
+
+  return (
+    <>
+      <Typography variant="h2" component="h2" fontFamily="Righteous">
+        Upcoming Movies
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid
+          item
+          xs={12}
+          md={3}
+          minHeight="100%"
+          maxWidth="100%"
+          sx={{ mb: 3, mt: 3 }}
+        >
+          <FilterMovies />
+        </Grid>
+        <Grid
+          container
+          xs={12}
+          md={9}
+          display="flex"
+          flex-wrap="wrap"
+          justifyContent="center"
+          gap="30px"
+          mt={3}
+        >
+          {movies.map((m) => (
+            <Grid item key={m.id}>
+              <MovieCard key={m.id} movie={m} />
+            </Grid>
+          ))}
+          <Grid item xs={12} sx={{ mt: 5 }}>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <MoviePagination />
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Footer />
+    </>
+  );
+};
+export default UpcomingMoviesPage;
